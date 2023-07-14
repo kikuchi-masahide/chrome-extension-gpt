aa1e2663c54d0326f8002aea29a5f201e85cf7fc
菊池雅英 <m20000406k@gmail.com>
2023-07-15 08:14:23 +0900

    ブックマーク内あいまい検索の機能実装

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,38 +1,79 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Configuration, OpenAIApi } from "openai";
+import { useList } from "react-use";
 
-const Popup = () => {
-    const [openai_api_key, set_openai_api_key] = useState("");
-    let openai: null | OpenAIApi = null;
+import { embed } from "../utils/embed";
+import { StorageLocalGet } from "../utils/storage_local";
+import BookmarkDataType from "../types/bookmark_data_type";
 
-    useEffect(() => {
-        SetOpenAIApiKey(set_openai_api_key);
-    });
-
-    //openai_api_keyが設定されたタイミングでopenaiを初期化する
-    useEffect(() => {
-        if (openai_api_key === "") return;
-        const configuration = new Configuration({
-            apiKey: openai_api_key,
-        });
-        openai = new OpenAIApi(configuration);
-    }, [openai_api_key]);
-
-    return <></>;
+const dot = (a: number[], b: number[]) => {
+    return a.map((x, i) => a[i] * b[i]).reduce((m, n) => m + n);
 };
 
-function SetOpenAIApiKey(api_key_setter: (api_key: string) => void) {
-    //get storaged value with key "openai_api_key" using chrome.sync.get
-    chrome.storage.sync.get(
-        {
-            openai_api_key: "",
-        },
-        (items) => {
-            api_key_setter(items["openai_api_key"]);
-        }
+const search = async (query: string) => {
+    console.log("searching for", query);
+    //embedding
+    const vector = await embed(query);
+    console.log("vector", vector);
+    //保存しているブックマークの集合
+    const storage_local = await StorageLocalGet(["saved_bookmarks"]);
+    const saved_bookmarks = storage_local.saved_bookmarks
+        ? (storage_local.saved_bookmarks as BookmarkDataType[])
+        : new Array<BookmarkDataType>();
+    const similarities: [BookmarkDataType, number][] = saved_bookmarks.map(
+        (bookmark) => [bookmark, dot(vector, bookmark.vector)]
     );
+    //similaritiesを類似度(第1要素)降順でソート
+    similarities.sort((a, b) => b[1] - a[1]);
+    return similarities.map((similarity) => similarity[0]);
+};
+const Popup = () => {
+    const [searchResults, setSearchResults] = useList<BookmarkDataType>();
+    const onSearchClick = (text: string) => {
+        search(text).then((results) => {
+            setSearchResults.set(results);
+        });
+    };
+    const containerStyle = {
+        width: "400px",
+        height: "400px",
+    };
+    return (
+        <>
+            <div className="container" style={containerStyle}>
+                <SearchTextbox onSearchClick={onSearchClick} />
+                {searchResults.map((bookmark) => (
+                    <p key={bookmark.id}>{bookmark.title}</p>
+                ))}
+            </div>
+        </>
+    );
+};
+
+interface SearchTextboxProps {
+    onSearchClick: (text: string) => void;
 }
+
+const SearchTextbox = ({ onSearchClick }: SearchTextboxProps) => {
+    const [text, setText] = useState("");
+    return (
+        <div className="input-group mb-3">
+            <input
+                type="text"
+                className="form-control"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={() => onSearchClick(text)}
+            >
+                検索
+            </button>
+        </div>
+    );
+};
 
 ReactDOM.render(
     <React.StrictMode>
